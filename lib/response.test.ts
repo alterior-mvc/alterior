5d32062fec78061a7ff4addc3f0518c70c1cc5b2
William Lahti <wilahti@gmail.com>
2016-09-25 18:43:00 -0400

    further tests on Response

--- lib/response.test.ts
+++ lib/response.test.ts
@@ -11,10 +11,47 @@ import { HttpException } from './errors';
 import { AppOptions } from './application';
 import { bootstrap } from './bootstrap';
 import * as supertest from 'supertest';
+import { expect } from 'chai';
 
 describe("response", () => {
 
 	describe("Response", () => {
+		it('.created() should produce a 201 Created with response URL', () => {
+			let body = {name: 'Foo'};
+			let response = _Response.created('http://example.com/', body);
+
+			expect(response.status).to.equal(201);
+			expect(response.headers[0][0]).to.equal('Location');
+			expect(response.headers[0][1]).to.equal('http://example.com/');
+			expect(response.body).to.equal(JSON.stringify(body));
+		});
+
+		it('string body should be JSON encoded', () => {
+			let body = {name: 'Foo'};
+			let response = new _Response(200, [], "hello");
+			expect(response.body).to.equal(`"hello"`);
+		});
+
+		it('.encodeAs(\'raw\') should cause body to be pass-through encoded', () => {
+			let body = {name: 'Foo'};
+			let response = new _Response(200, [], "hello").encodeAs('raw');
+			expect(response.body).to.equal("hello");
+		});
+
+		it('.throw() should throw an equivalent HttpException', () => {
+			let body = {name: 'Foo'};
+			try {
+				new _Response(123, [['X-Test', 'pass']], "hello").throw();
+			} catch (e) {
+				expect(e).to.be.an.instanceof(HttpException);
+				let httpe = <HttpException>e;
+				expect(httpe.body).to.equal(`"hello"`);
+				expect(httpe.statusCode).to.equal(123);
+				expect(httpe.headers.length).to.equal(1);
+				expect(httpe.headers[0][0]).to.equal('X-Test');
+				expect(httpe.headers[0][1]).to.equal('pass');
+			}
+		});
 		
 		it('should be accepted and used when given as return value of a controller route method', (done) => {
     
