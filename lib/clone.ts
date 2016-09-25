eb675a886b404eb49d75f4bf00b00f83d41ddae8
William Lahti <wilahti@gmail.com>
2016-09-25 19:50:39 -0400

    shallowClone() clones 1 level, preserves functions

--- lib/clone.ts
+++ lib/clone.ts
@@ -1,3 +1,13 @@
+
+export function shallowClone(obj : Object) {
+	if (typeof obj !== 'object')
+		return obj;
+	
+	let clone = Object.create(obj.constructor.prototype);
+	Object.assign(clone, obj);
+	return clone;
+}
+
 export function clone(obj) {
 	return JSON.parse(JSON.stringify(obj));
 }
