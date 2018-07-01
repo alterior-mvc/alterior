69f3527da14091ba065336b7457e563e77bb2353
William Lahti <wilahti@gmail.com>
2018-06-30 20:05:19 -0700

    back to es5 for main build for now, but introduces ESM build

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./lib/accesscontrol"));
+__export(require("./lib/controller"));
+__export(require("./lib/application"));
+__export(require("./lib/express"));
+__export(require("./lib/middleware"));
+__export(require("./lib/route"));
+__export(require("./lib/bootstrap"));
+__export(require("./lib/errors"));
+__export(require("./lib/response"));
+//# sourceMappingURL=index.js.map
