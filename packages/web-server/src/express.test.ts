4ec924e967da9b6d15c324f57aed5d303c4ca9be
William Lahti <wilahti@gmail.com>
2019-03-01 20:31:12 -0800

    dont fail just because theres more than one module loaded

--- packages/web-server/src/express.test.ts
+++ packages/web-server/src/express.test.ts
@@ -2,13 +2,14 @@ import { suite } from 'razmin';
 import { ExpressRef as _ExpressRef } from './express-ref';
 import { AppOptions, Application } from '@alterior/runtime';
 import * as assert from 'assert';
+import { expect } from 'chai';
 import { Module } from '@alterior/di';
 import { WebServerModule } from './web-server.module';
 
 suite(describe => {
 	describe('ExpressRef', it => {
 		it('should provide ExpressRef with a valid Express application in it', async () => {
-			
+
 			return new Promise(async (resolve, reject) => {
 				@Module({
 					imports: [
@@ -23,12 +24,17 @@ suite(describe => {
 				}
 
 				let app = await Application.bootstrap(FakeApp, { autostart: false });
-				let module = app.runtime.instances[0].instance;
+				let module = app.runtime.instances.find(x => x.instance.constructor === FakeApp).instance;
+
 				let expressRef = module.expressRef;
+
+				expect(expressRef, 'ExpressRef is invalid').to.exist;
+
 				let expressApp = expressRef.application;
 
-				assert(expressApp, 'ExpressRef provided invalid app value');
-				assert(expressApp.patch, 'ExpressRef provided app without patch() method');
+				expect(expressApp, 'ExpressRef provided invalid app value').to.exist;
+				expect(expressApp.patch, 'ExpressRef provided app without patch() method').to.exist;
+
 				resolve();
 
 				app.stop();
