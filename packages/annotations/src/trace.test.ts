f18287f01b5137cd92680623074bae97a236c677
William Lahti <wilahti@gmail.com>
2019-03-16 02:25:46 -0700

    syntax + working trace test

---
+++ packages/annotations/src/trace.test.ts
@@ -0,0 +1,49 @@
+import { suite } from "razmin";
+import { ConsoleTrace } from "./trace";
+import { interceptConsole } from "@alterior/common";
+import { expect } from 'chai';
+
+suite(describe => {
+    describe('Trace', it => {
+        it('prints a trace message', () => {
+            let log = [];
+
+            interceptConsole((method, original, console, args) => {
+                log.push({ method, original, console, args });
+            }, () => {
+                class Thing {
+                    @ConsoleTrace()
+                    static doSomething(data : any, stuff : number) {
+                        console.log("Doing something...");
+                        try {
+                            this.doAnotherThing(stuff);
+                        } catch (e) {
+    
+                        }
+                    }
+    
+                    @ConsoleTrace()
+                    static doAnotherThing(stuff : number) {
+                        console.log("Doing another thing...");
+                        throw new Error('Uh oh');
+                    }
+                }
+    
+                let thing = new Thing();
+                Thing.doSomething({ stuff: 321, other: "nice" }, 12333); 
+            });
+
+            expect(log.length).to.eq(8);
+
+            expect(log[0].args[0]).to.contain('{');
+            expect(log[1].args[0]).to.contain('Doing something...');
+
+            expect(log[2].args[0]).to.contain('{');
+            expect(log[3].args[0]).to.contain('Doing another thing...');
+            expect(log[4].args[0]).to.contain('Exception');
+            expect(log[5].args[0]).to.contain('Error: Uh oh');
+            expect(log[6].args[0]).to.contain('}');
+            expect(log[7].args[0]).to.contain('}');
+        });
+    });
+});
--- packages/annotations/src/trace.ts
+++ packages/annotations/src/trace.ts
@@ -201,4 +201,4 @@ export function ReportExceptionsToConsole() {
             };
         }
     });
-);
+}
