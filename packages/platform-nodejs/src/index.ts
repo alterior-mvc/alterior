debcdeb5783d8b397d1a99968a2221191b21edcc
William Lahti <wilahti@gmail.com>
2021-10-25 02:16:31 -0700

    use import to handle fetch polyfill

--- packages/platform-nodejs/src/index.ts
+++ packages/platform-nodejs/src/index.ts
@@ -3,5 +3,7 @@ import "dotenv/config";
 import "source-map-support/register";
 import "reflect-metadata";
 
+import fetch from "node-fetch";
+
 if (!globalThis.fetch)
-    globalThis.fetch = require('node-fetch');
+    globalThis.fetch = <typeof globalThis.fetch><unknown>fetch;
