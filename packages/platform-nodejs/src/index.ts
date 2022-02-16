6d69efc6a1db64d22228cb26243e5dfb46dbd642
William Lahti <wilahti@gmail.com>
2022-02-16 01:34:50 -0800

    fix for reverse logic

--- packages/platform-nodejs/src/index.ts
+++ packages/platform-nodejs/src/index.ts
@@ -8,7 +8,7 @@ import WebSocket from 'ws';
 
 if (!globalThis.fetch) {
     globalThis.fetch = <typeof globalThis.fetch><unknown>fetch;
-    if (globalThis.Response)
+    if (!globalThis.Response)
         globalThis.Response = fetch.Response;
     else
         console.warn(`While polyfilling fetch(): globalThis.Response is already defined! Some features of Alterior may not work correctly.`);
