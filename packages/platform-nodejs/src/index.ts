86d7668ac831d3af0f13e646ef0cec45ab3df62a
William Lahti <wilahti@gmail.com>
2022-02-16 00:41:12 -0800

    also polyfill Response

--- packages/platform-nodejs/src/index.ts
+++ packages/platform-nodejs/src/index.ts
@@ -6,8 +6,13 @@ import "reflect-metadata";
 import fetch from "node-fetch";
 import WebSocket from 'ws';
 
-if (!globalThis.fetch)
+if (!globalThis.fetch) {
     globalThis.fetch = <typeof globalThis.fetch><unknown>fetch;
+    if (globalThis.Response)
+        globalThis.Response = fetch.Response;
+    else
+        console.warn(`While polyfilling fetch(): globalThis.Response is already defined! Some features of Alterior may not work correctly.`);
+}
 
 if (!globalThis.WebSocket)
     globalThis.WebSocket = WebSocket;
