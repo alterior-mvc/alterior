7f081cdcebd2feea90d9aebb33622014e3519cd1
William Lahti <wilahti@gmail.com>
2023-01-02 15:18:15 -0800

    support .env files above the current directory

--- packages/platform-nodejs/src/index.ts
+++ packages/platform-nodejs/src/index.ts
@@ -1,5 +1,33 @@
 import "zone.js";
-import "dotenv/config";
+import dotenv from "dotenv";
+import fs from 'fs';
+import path from 'path';
+
+function fileExists(file: string): boolean {
+    try {
+        let stat = fs.statSync(file);
+        return stat.isFile();
+    } catch (e) {
+        return false;
+    }
+}
+
+function findDotEnv() {
+    let dir = process.cwd();
+    while (!fileExists(path.join(dir, '.env'))) {
+        let parentDir = path.resolve(dir, '..');
+        if (dir === parentDir)
+            return undefined;
+        dir = parentDir;
+    }
+
+    return path.resolve(dir, '.env');
+}
+
+let dotEnvPath = findDotEnv();
+if (dotEnvPath)
+    dotenv.config({ path: findDotEnv() });
+
 import "source-map-support/register";
 import "reflect-metadata";
 
