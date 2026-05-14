1588b2742e6a4e4fbc51f6dc7bba9e7ccc047424
William Lahti <wilahti@gmail.com>
2026-05-13 17:23:28 -0700

    fixes for fastify@5

--- packages/fastify/src/fastify-engine.ts
+++ packages/fastify/src/fastify-engine.ts
@@ -1,48 +1,55 @@
-import fastify, { FastifyInstance, FastifyReply } from "fastify";
-import { WebEvent, WebServerEngine, ConnectMiddleware, ServerOwnedWebEvent, WebServer, ConnectApplication, RequestBase, ResponseBase } from '@alterior/web-server';
+import middie from '@fastify/middie';
+import fastify from "fastify";
 
-import * as http from 'http';
 import { inject } from "@alterior/di";
-
-export type FastifyConnectMiddleware = ConnectMiddleware & fastify.FastifyInstance<http.Server, http.IncomingMessage, http.ServerResponse>;
+import {
+    WebEvent, WebServerEngine, ConnectMiddleware, ServerOwnedWebEvent, WebServer, ConnectApplication,
+    RequestBase, ResponseBase
+} from '@alterior/web-server';
 
 export class FastifyEngine extends WebServerEngine {
     private server = inject(WebServer);
-    
-	readonly fastify = <FastifyConnectMiddleware & FastifyInstance>fastify();
-	readonly app = this.fastify as ConnectApplication;
-	readonly providers = [];
 
-	override sendJsonBody(routeEvent : WebEvent, body : any) {
-		routeEvent.response.setHeader('Content-Type', 'application/json');
-		(routeEvent.response as unknown as FastifyReply<http.ServerResponse>).send(body);
-	}
+    private setup() {
+        let app = fastify();
+        app.register(middie);
+        return app;
+    }
+    readonly fastify = this.setup();
+    readonly app: ConnectApplication = (req, res) => this.fastify.server.emit('request', req, res);
+    readonly providers = [];
 
-	addConnectMiddleware(path : string, middleware : any) {
-		this.fastify.use(path, middleware);
-	}
-	
-	addRoute(method : string, path : string, handler : (event : WebEvent) => void, middleware?: ConnectMiddleware[]) {
-		if (!middleware)
-			middleware = [];
-		(this.fastify as any)[this.getRegistrarName(method)](
-			path, 
-			...middleware, 
-			(req: RequestBase, res: ResponseBase) => {
-				return handler(new WebEvent(req, res));
-			}
-		);
-	}
+    override sendJsonBody(routeEvent: WebEvent, body: any) {
+        routeEvent.response.setHeader('Content-Type', 'application/json');
+        routeEvent.response.write(body);
+        routeEvent.response.end();
+    }
 
-	addAnyRoute(handler : (event : ServerOwnedWebEvent) => void) {
-		this.fastify.use((req, res) => handler(this.server.registerEvent(new WebEvent(<any>req, <any>res))));
-	}
+    override addConnectMiddleware(path: string, middleware: any) {
+        this.fastify.use(path, middleware);
+    }
 
-	private getRegistrarName(method : string) {
-		let registrar = method.toLowerCase();
-		if (!this.supportedMethods.includes(registrar))
-			throw new Error(`The specified method '${method}' is not supported by Express.`);
-			
-		return registrar;
-	}
+    override addRoute(method: string, path: string, handler: (event: WebEvent) => void, middleware?: ConnectMiddleware[]) {
+        if (!middleware)
+            middleware = [];
+        (this.fastify as any)[this.getRegistrarName(method)](
+            path,
+            ...middleware,
+            (req: RequestBase, res: ResponseBase) => {
+                return handler(new WebEvent(req, res));
+            }
+        );
+    }
+
+    override addAnyRoute(handler: (event: ServerOwnedWebEvent) => void) {
+        this.fastify.all('*', (req, res) => handler(this.server.registerEvent(new WebEvent(req as any, res as any))));
+    }
+
+    private getRegistrarName(method: string) {
+        let registrar = method.toLowerCase();
+        if (!this.supportedMethods.includes(registrar))
+            throw new Error(`The specified method '${method}' is not supported by Express.`);
+
+        return registrar;
+    }
 }
